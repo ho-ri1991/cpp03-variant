1520ef978c6cab30b2a3af017ecdccb55d5733c8
ho-ri1991 <hage.1991@hotmail.co.jp>
2018-06-24 23:07:14 +0900

    add true_type and false_type

--- type_traits.hpp
+++ type_traits.hpp
@@ -4,6 +4,9 @@
 namespace my {
 namespace type_traits {
 
+  struct true_type { enum { value = 1 }; };
+  struct false_type { enum { value = 0 }; };
+
   template <class T, class U>
   struct is_same { enum { value = 0 }; };
   template <class T>
