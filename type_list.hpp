5948774d7d641a8afbc832d7be9304a14c2abafb
ho-ri1991 <hage.1991@hotmail.co.jp>
2018-06-29 15:10:26 +0900

    fix compile error in type_list macro

--- type_list.hpp
+++ type_list.hpp
@@ -46,18 +46,18 @@ namespace type_traits {
 
 }}
 
-#define MAKE_MY_TYPE_LIST_1(T1) my::type_traits::type_list<T1, null_type>
-#define MAKE_MY_TYPE_LIST_2(T1, T2) my::type_traits::type_list<T1, MAKE_MY_TYPE_LIST_1(T2) >
-#define MAKE_MY_TYPE_LIST_3(T1, T2, T3) my::type_traits::type_list<T1, MAKE_MY_TYPE_LIST_2(T2, T3) >
-#define MAKE_MY_TYPE_LIST_4(T1, T2, T3, T4) my::type_traits::type_list<T1, MAKE_MY_TYPE_LIST_3(T2, T3, T4) >
-#define MAKE_MY_TYPE_LIST_5(T1, T2, T3, T4, T5) my::type_traits::type_list<T1, MAKE_MY_TYPE_LIST_4(T2, T3, T4, T5) >
-#define MAKE_MY_TYPE_LIST_6(T1, T2, T3, T4, T5, T6) my::type_traits::type_list<T1, MAKE_MY_TYPE_LIST_5(T2, T3, T4, T5, T6) >
-#define MAKE_MY_TYPE_LIST_7(T1, T2, T3, T4, T5, T6, T7) my::type_traits::type_list<T1, MAKE_MY_TYPE_LIST_6(T2, T3, T4, T5, T6, T7) >
-#define MAKE_MY_TYPE_LIST_8(T1, T2, T3, T4, T5, T6, T7, T8) my::type_traits::type_list<T1, MAKE_MY_TYPE_LIST_7(T2, T3, T4, T5, T6, T7, T8) >
-#define MAKE_MY_TYPE_LIST_9(T1, T2, T3, T4, T5, T6, T7, T8, T9) my::type_traits::type_list<T1, MAKE_MY_TYPE_LIST_8(T2, T3, T4, T5, T6, T7, T8, T9) >
-#define MAKE_MY_TYPE_LIST_10(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10) my::type_traits::type_list<T1, MAKE_MY_TYPE_LIST_9(T2, T3, T4, T5, T6, T7, T8, T9, T10) >
-#define MAKE_MY_TYPE_LIST_11(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11) my::type_traits::type_list<T1, MAKE_MY_TYPE_LIST_10(T2, T3, T4, T5, T6, T7, T8, T9, T10, T11) >
-#define MAKE_MY_TYPE_LIST_12(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12) my::type_traits::type_list<T1, MAKE_MY_TYPE_LIST_11(T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12) >
+#define MAKE_MY_TYPE_LIST_1(T1) ::my::type_traits::type_list<T1, ::my::type_traits::null_type>
+#define MAKE_MY_TYPE_LIST_2(T1, T2) ::my::type_traits::type_list<T1, MAKE_MY_TYPE_LIST_1(T2) >
+#define MAKE_MY_TYPE_LIST_3(T1, T2, T3) ::my::type_traits::type_list<T1, MAKE_MY_TYPE_LIST_2(T2, T3) >
+#define MAKE_MY_TYPE_LIST_4(T1, T2, T3, T4) ::my::type_traits::type_list<T1, MAKE_MY_TYPE_LIST_3(T2, T3, T4) >
+#define MAKE_MY_TYPE_LIST_5(T1, T2, T3, T4, T5) ::my::type_traits::type_list<T1, MAKE_MY_TYPE_LIST_4(T2, T3, T4, T5) >
+#define MAKE_MY_TYPE_LIST_6(T1, T2, T3, T4, T5, T6) ::my::type_traits::type_list<T1, MAKE_MY_TYPE_LIST_5(T2, T3, T4, T5, T6) >
+#define MAKE_MY_TYPE_LIST_7(T1, T2, T3, T4, T5, T6, T7) ::my::type_traits::type_list<T1, MAKE_MY_TYPE_LIST_6(T2, T3, T4, T5, T6, T7) >
+#define MAKE_MY_TYPE_LIST_8(T1, T2, T3, T4, T5, T6, T7, T8) ::my::type_traits::type_list<T1, MAKE_MY_TYPE_LIST_7(T2, T3, T4, T5, T6, T7, T8) >
+#define MAKE_MY_TYPE_LIST_9(T1, T2, T3, T4, T5, T6, T7, T8, T9) ::my::type_traits::type_list<T1, MAKE_MY_TYPE_LIST_8(T2, T3, T4, T5, T6, T7, T8, T9) >
+#define MAKE_MY_TYPE_LIST_10(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10) ::my::type_traits::type_list<T1, MAKE_MY_TYPE_LIST_9(T2, T3, T4, T5, T6, T7, T8, T9, T10) >
+#define MAKE_MY_TYPE_LIST_11(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11) ::my::type_traits::type_list<T1, MAKE_MY_TYPE_LIST_10(T2, T3, T4, T5, T6, T7, T8, T9, T10, T11) >
+#define MAKE_MY_TYPE_LIST_12(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12) ::my::type_traits::type_list<T1, MAKE_MY_TYPE_LIST_11(T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12) >
 
 #endif
 
